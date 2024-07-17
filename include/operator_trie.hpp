986dcc355f77584269d5a3aa378de4c71b209622
malevitzch <malevitzch@gmail.com>
2024-07-17 14:22:28 +0200

    Improved the OperatorTrie and TrieNode classes through the use of shared_ptr

--- include/operator_trie.hpp
+++ include/operator_trie.hpp
@@ -1,14 +1,22 @@
 #include <string>
 #include <vector>
+#include <memory>
 using std::string;
+#define ALPHABET_SIZE 26
+
 class OperatorTrie
-{  
+{ 
 private:
-  friend class TreeNode;
+  friend class TrieNode;
   class TrieNode
   {
-    
+  private:
+    std::shared_ptr<int> ptrs[ALPHABET_SIZE];
+    bool finish;
+  public:
+    friend class OperatorTrie;
   };
+  std::shared_ptr<TrieNode> root;
 public:
   std::vector<string> split_string(string str);
   void add_operator(string op);
