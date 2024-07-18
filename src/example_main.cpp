b89d32e1de2e4a340a8f8214d5cc033bb944ef00
malevitzch <malevitzch@gmail.com>
2024-07-18 16:43:55 +0200

    Added temporary tests

--- src/example_main.cpp
+++ src/example_main.cpp
@@ -2,5 +2,15 @@
 #include "operator_trie.hpp"
 int main()
 {
-  std::cout<<"Compiled successfully\n";
+  std::vector<char> alphabet;
+  for(char ch = 'a'; ch <= 'z'; ch++)
+  {
+    alphabet.push_back(ch);
+  }
+  Trie trie(alphabet);
+  trie.insert("abc");
+  trie.insert("abb");
+  std::cout<< trie.contains("abc") << "\n";
+  std::cout<< trie.contains("abcd") << "\n";
+  std::cout<< trie.contains("abb") << "\n"; 
 }
